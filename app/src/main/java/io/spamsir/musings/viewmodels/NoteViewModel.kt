75f23f8e4a67ab0e8410d08b0119d815dad33388
stbeaumont98 <spence.beaumont@gmail.com>
2024-03-11 10:09:54 -0400

    Starting to get NoteListItem working again.

--- app/src/main/java/io/spamsir/musings/viewmodels/NoteViewModel.kt
+++ app/src/main/java/io/spamsir/musings/viewmodels/NoteViewModel.kt
@@ -6,15 +6,30 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.app.AlarmManagerCompat
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
+import dagger.hilt.android.lifecycle.HiltViewModel
 import io.spamsir.musings.MusingsApplication
+import io.spamsir.musings.NoteState
 import io.spamsir.musings.data.Note
+import io.spamsir.musings.database.NoteDatabase
 import io.spamsir.musings.database.NoteDatabaseDao
 import io.spamsir.musings.notifications.NotificationsReceiver
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class NoteViewModel(private val noteDao: NoteDatabaseDao) : ViewModel() {
+@HiltViewModel
+class NoteViewModel @Inject constructor(
+    private val noteDao: NoteDatabaseDao
+) : ViewModel() {
 
-
-
+    private val mutableState = MutableStateFlow(NoteState())
+    val state = mutableState.asStateFlow()
     fun updateNote(key: Long, isLiked: Boolean) = noteDao.update(key, isLiked)
     fun getNote(key: Long) : Note = noteDao.get(key)
 
@@ -24,6 +39,17 @@ class NoteViewModel(private val noteDao: NoteDatabaseDao) : ViewModel() {
 
     private val alarmManager = MusingsApplication.applicationContext().getSystemService(Context.ALARM_SERVICE) as AlarmManager
     private val notifyIntent = Intent(MusingsApplication.applicationContext(), NotificationsReceiver::class.java)
+
+    fun loadData(key: Long) {
+        viewModelScope.launch(Dispatchers.IO) {
+            mutableState.value = mutableState.value.copy(
+                dateTime = noteDao.get(key).dateTime,
+                title = noteDao.get(key).title,
+                content = noteDao.get(key).content,
+                isLiked = noteDao.get(key).isLiked
+            )
+        }
+    }
 
     init {
         notifyPendingIntent = PendingIntent.getBroadcast(
@@ -44,12 +70,16 @@ class NoteViewModel(private val noteDao: NoteDatabaseDao) : ViewModel() {
     }
 
     companion object {
-        @Volatile
-        private var instance: NoteViewModel? = null
+        val Factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+                val application = checkNotNull(extras[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY])
+                val dataSource = NoteDatabase.getInstance(application.applicationContext, CoroutineScope(
+                    Dispatchers.IO)
+                ).noteDatabaseDao
 
-        fun getInstance(noteDao: NoteDatabaseDao) =
-            instance ?: synchronized(this) {
-                instance ?: NoteViewModel(noteDao).also { instance = it }
+                return AllNotesViewModel(dataSource) as T
             }
+        }
     }
 }
